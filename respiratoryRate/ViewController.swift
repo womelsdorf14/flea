de4b97a3ccf46cdd8caa0375bd4f2e1c4a9b6ed0
Leah Womelsdorf <lew96@cornell.edu>
2018-08-27 17:39:27 -0400

    Start/stop
    Changed interface so just start/stop then automatically sends to dropBox

--- respiratoryRate/ViewController.swift
+++ respiratoryRate/ViewController.swift
@@ -16,132 +16,8 @@ class ViewController: UIViewController, WCSessionDelegate{
     
 // TODO: Change to your OAuth2 token to send to your Dropbox
     var client = DropboxClient(accessToken: "O0rjYSZUS9AAAAAAAAACTlsFwgxufsT5OkM5oa7wImJIbkAIQr6f7r3xf6AzQQ3g")
-    
-    // Size of window for averaging filter
-    // Size of 150 based on 1 breath for max breathing rate 40 bpm (1.5 s/breath, 100 fps)
-    let windowSize = 150
-    
-    var zScores: [[Double]] = [[], [], [], []]
-    @IBAction func makeZ(_ sender: Any) {
-        let count = arrGy[0].count
-        var sumX = 0.0
-        var sumY = 0.0
-        var sumZ = 0.0
-        for i in 0..<count {
-            sumX = sumX + arrGy[1][i]
-            sumY = sumY + arrGy[2][i]
-            sumZ = sumZ + arrGy[3][i]
-        }
-        let meanX = sumX/Double(count)
-        let meanY = sumY/Double(count)
-        let meanZ = sumZ/Double(count)
-        var stdX = 0.0
-        var stdY = 0.0
-        var stdZ = 0.0
-        for i in 0..<count {
-            let powX = pow((arrGy[1][i] - meanX), 2)
-            let powY = pow((arrGy[2][i] - meanY), 2)
-            let powZ = pow((arrGy[3][i] - meanZ), 2)
-            stdX = stdX + powX
-            stdY = stdY + powY
-            stdZ = stdZ + powZ
-        }
-        stdX = pow((stdX / Double(count+1)), 0.5)
-        stdY = pow((stdY / Double(count+1)), 0.5)
-        stdZ = pow((stdZ / Double(count+1)), 0.5)
-        for entry in arrGy[0] as NSArray {
-            self.zScores[0].append(entry as! Double)
-        }
-        for entry in arrGy[1] as NSArray {
-            let zX = ((entry as! Double) - meanX)/stdX
-            self.zScores[1].append(zX)
-        }
-        for entry in arrGy[2] as NSArray {
-            let zY = ((entry as! Double) - meanY)/stdY
-            self.zScores[2].append(zY)
-        }
-        for entry in arrGy[3] as NSArray {
-            let zZ = ((entry as! Double) - meanZ)/stdZ
-            self.zScores[3].append(zZ)
-        }
-    }
-    
-    var avgWin: [[Double]] = [[], [], [], []]
-    @IBAction func makeA(_ sender: Any) {
-        let count = arrGy[0].count
-        
-        for i in 0..<(count-self.windowSize) {
-            var sumX = 0.0
-            var sumY = 0.0
-            var sumZ = 0.0
-            for j in i..<(i+self.windowSize) {
-                sumX = sumX + zScores[1][j]
-                sumY = sumY + zScores[2][j]
-                sumZ = sumZ + zScores[3][j]
-            }
-            let avX = sumX/Double(self.windowSize)
-            let avY = sumY/Double(self.windowSize)
-            let avZ = sumZ/Double(self.windowSize)
-            avgWin[0].append(arrGy[0][i])
-            avgWin[1].append(avX)
-            avgWin[2].append(avY)
-            avgWin[3].append(avZ)
-        }
-        
 
-        
-        
-    }
-    
-    @IBAction func sendZ(_ sender: Any) {
-        let time = "\(CFAbsoluteTimeGetCurrent())"
-        var csvText = "Time,zscoreX,zscoreY,zscoreZ\n"
-        let count = zScores[0].count
-        for i in 0..<count {
-            let newLine = "\(zScores[0][i]),\(zScores[1][i]),\(zScores[2][i]),\(zScores[3][i])\n"
-            csvText.append(newLine)
-        }
-        let fileData = csvText.data(using: String.Encoding.utf8, allowLossyConversion: false)!
-        client.files.upload(path: "/respiratoryRate/zScores/\(time).csv", input: fileData)
-            .response { response, error in
-                if let response = response {
-                    print(response)
-                } else if let error = error {
-                    print(error)
-                }
-            }
-            .progress { progressData in
-                print(progressData)
-        }
-    }
-    
-    @IBAction func sendA(_ sender: Any) {
-        let time = "\(CFAbsoluteTimeGetCurrent())"
-        var csvText = "Time,zscoreX,zscoreY,zscoreZ\n"
-        let count = avgWin[0].count
-        for i in 0..<count {
-            let newLine = "\(avgWin[0][i]),\(avgWin[1][i]),\(avgWin[2][i]),\(avgWin[3][i])\n"
-            csvText.append(newLine)
-        }
-        let fileData = csvText.data(using: String.Encoding.utf8, allowLossyConversion: false)!
-        client.files.upload(path: "/respiratoryRate/avGs/\(time).csv", input: fileData)
-            .response { response, error in
-                if let response = response {
-                    print(response)
-                } else if let error = error {
-                    print(error)
-                }
-            }
-            .progress { progressData in
-                print(progressData)
-        }
-    }
-    
-    @IBAction func sendG(_ sender: Any) {
-        self.toDrop_G()
-    }
-    
-    func toDrop_G() {
+    func send_G() {
         let time = "\(CFAbsoluteTimeGetCurrent())"
         var csvText = "Time,gyX,gyY,gyZ\n"
         let count = arrGy[0].count
@@ -163,9 +39,7 @@ class ViewController: UIViewController, WCSessionDelegate{
         }
     }
     
-
-    
-
+   
     @IBOutlet var chtChart: LineChartView!
     
     func updateGraph() {
@@ -197,7 +71,6 @@ class ViewController: UIViewController, WCSessionDelegate{
         data.addDataSet(line3)
         
         chtChart.data = data
-        
         chtChart.chartDescription?.text = "My awesome chart"
     }
     
@@ -214,29 +87,18 @@ class ViewController: UIViewController, WCSessionDelegate{
             session?.activate()
         }
     }
-    
-    @IBAction func resetArrGy(_ sender: Any) {
-        arrGy = [[], [], [], []]
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBOutlet weak var arrayCount: UILabel!
     
-    @IBOutlet weak var times: UILabel!
-    @IBOutlet weak var xGy: UILabel!
-    @IBOutlet weak var yGy: UILabel!
-    @IBOutlet weak var zGy: UILabel!
-    
-    var myData : [[Double]] = [[], [], [], []]
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
         DispatchQueue.main.async() {
             // Instantaneous update
             let array = message["data"] as! NSArray
-            self.count = self.count + 1
+            self.count = array.count
             self.countL.text = "\(self.count)"
             for entry in array[0] as! NSArray {
                 self.arrGy[0].append(entry as! Double)
@@ -251,27 +113,14 @@ class ViewController: UIViewController, WCSessionDelegate{
                 self.arrGy[3].append(entry as! Double)
             }
             self.updateGraph()
-            if self.count > 19 {
-//                self.makeZ()
-   //             self.makeAvg()
-         //       self.toDrop_G()
-//                self.toDrop_Z()
-//                self.toDrop_A()
-                self.count = 0
-            }
+            self.send_G()
         }
+        
     }
     
-    @IBAction func dropZ(_ sender: Any) {
-   //     self.toDrop_Z()
-    }
-    
-    @IBAction func dropAvg(_ sender: Any) {
-       // self.toDrop_A()
-    }
 
-    
-    
+
+
     
     
     func session(_ session: WCSession,
@@ -281,7 +130,6 @@ class ViewController: UIViewController, WCSessionDelegate{
     func sessionDidBecomeInactive(_ session: WCSession) {}
     
     func sessionDidDeactivate(_ session: WCSession) {}
-    
     
 }
 
